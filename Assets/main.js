f3799c881a83cb67ca4b236ccb51d8b9927bb97e
Stephen H <Sthogue16@gmail.com>
2023-06-22 16:38:29 -0500

    Initial Commit
    Includes basic HTML, CSS, and start of JS Code

---
+++ Assets/main.js
@@ -0,0 +1,64 @@
+// Quiz Inputs
+// Start button
+// Question answers
+// score input
+// reset high scores  
+
+// Basic structure
+// quiz questions location
+// quiz question answers
+// user answer
+// correct answer
+// total correct
+// user high scores
+// 
+
+var questionBank= [
+    { 
+        question: "Are Java and Javascript the same computer language?",
+        a: "Yes",
+        b: "No",
+        correct: "b"
+    },
+    {
+        question: "Commonly used data types do not include?",
+        a: "Strings",
+        b: "Booleans",
+        c: "Alerts",
+        d: "Numbers",
+        correct: "c",
+    },
+    {
+        question: "The condition in an if/else statement is enclosed with ______ ?",
+        a: "Quotes",
+        b: "Curly Brackets",
+        c: "Parenthesis",
+        d: "Square Brackets",
+        correct: "c",
+    },
+    {
+        question: "Arrays in JavaScript can be used to store?",
+        a: "Numbers & Strings",
+        b: "Other arrays",
+        c: "Booleans",
+        d: "All of the above",
+        correct: "d",
+    },
+    {
+        question: "Arrays in JavaScript can be used to store?",
+        a: "Numbers & Strings",
+        b: "Other arrays",
+        c: "Booleans",
+        d: "All of the above",
+        correct: "d",
+    },
+    {
+        question:
+          "A very useful tool used during development and debugging for printing content to the debugger is?",
+        a: "console.log",
+        b: "Terminal/Bash",
+        c: "For loops",
+        d: "JavaScript",
+        correct: "a",
+      },
+];
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Coding-Quiz
 Week 4 Challenge. Users will be able to test their coding knowledge. 
+
+
+Background image credit: https://www.flickr.com/photos/132889348@N07/20607150556
